03027d5145023f78ea88527aca9fdaddd54332fe
sinovic1 <houhasensin00@gmail.com>
2025-05-25 23:39:30 +0100

    Create logic.py

---
+++ logic.py
@@ -0,0 +1,11 @@
+def check_signals():
+    return {
+        "EURUSD": {
+            "signal": "BUY",
+            "entry": 1.0850,
+            "sl": 1.0830,
+            "tp1": 1.0870,
+            "tp2": 1.0890,
+            "tp3": 1.0910
+        }
+    }
